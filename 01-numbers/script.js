cc99cb884f1f4b60303d9f3252dd977acd30dcd5
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2025-11-30 10:06:49 +0100

    esercizio 1 terminato

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -7,24 +7,24 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 
 // 1. Imposta questa variabile come somma di numOne e numTwo
 const numOnePlusNumTwo = numOne + numTwo;
-console.log(numOnePlusNumTwo); // Output: 24
+console.log(numOnePlusNumTwo); // Output: 24 ok
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
 const numThreeTimesNumTwo = numThree * numTwo;
-console.log(numThreeTimesNumTwo); // Output: 512
+console.log(numThreeTimesNumTwo); // Output: 512 ok
 
 // 3. Imposta questa variabile come divisione di numThree per numOne
 const numThreeDividedByNumOne = numThree / numOne;
-console.log(numThreeDividedByNumOne); // Output: 4
+console.log(numThreeDividedByNumOne); // Output: 4 ok
 
 // 4. Imposta questa variabile come differenza tra numThree e numOne
 const numThreeMinusNumOne = numThree - numOne;
-console.log(numThreeMinusNumOne); // Output: 24
+console.log(numThreeMinusNumOne); // Output: 24 ok
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
 const sum = numOne + numTwo + numThree;
-console.log(sum); // Output: 56
+console.log(sum); // Output: 56 ok
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
 const numBytes = (numOne + numTwo + numThree) / numOne;
-console.log(numBytes); // Output: 7
+console.log(numBytes); // Output: 7 ok
